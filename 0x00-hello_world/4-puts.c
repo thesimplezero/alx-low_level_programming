0a5a13c9324d82c743240169a6dd1104b57dd16a
thesimplezero <dvoidhunter@gmail.com>
2023-02-16 09:30:25 -0800

    futurama

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description: Prints a message using the 'put' function.
+ *
+ * Return: Always returns 0 to indicate success.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
