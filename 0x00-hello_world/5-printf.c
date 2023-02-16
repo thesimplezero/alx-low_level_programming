7d4b3e05b3090ddf8e95b842e81f20109c759668
thesimplezero <dvoidhunter@gmail.com>
2023-02-16 10:11:35 -0800

    a leap of faith

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a message using proper grammar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	return printf("with proper grammar, but the outcome is a piece of art,\n");
+}
