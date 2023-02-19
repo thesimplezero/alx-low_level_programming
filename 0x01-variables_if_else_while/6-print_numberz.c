da1790db78647927894e6a0fc81979fa68cc7c89
thesimplezero <hi@thesimplezero>
2023-02-19 16:39:41 +0300

    nx

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print 0-9 using putchar
- *              while using int variable
+ * Description: print all single digit numbers
  *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
 	int digit = 0;
 
 	while (digit <= 9)
 	{
-		/*convert digit to ASCII representation*/
 		putchar(digit + '0');
 		++digit;
 	}
