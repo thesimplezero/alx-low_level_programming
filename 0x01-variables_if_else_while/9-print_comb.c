f0e48e791117d46f0225850965cb98157d49a145
thesimplezero <hi@thesimplezero>
2023-02-19 16:52:38 +0300

    slow is good

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,29 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print 0, 1, - 9
+ * Description: prints all possible combinations of single-digit numbers
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	int digit = 0;
+	int i, j;
 
-	while (digit <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(digit + 48);
-
-		if (digit != 9)
+		for (j = i + 1; j < 10; j++)
 		{
+			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
-		}
+			putchar(j + '0');
 
-		++digit;
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
+
 	putchar('\n');
 
 	return (0);
