df0e8d5c6b6fdb1dde889ad36983adbf23fda25c
thesimplezero <hi@thesimplezero>
2023-02-20 23:24:02 +0300

    nexus loop

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-	int digit = 48; /*48; decimal rep of 0*/
+	int numberz = 48; /*48; decimal rep of 0*/
 
-	while (digit <= 102) /*102; decimal rep of f*/
+	while (numberz <= 102) /*102; decimal rep of f*/
 	{
-		putchar(digit);
+		putchar(numberz);
 
 		/* after 9 we jump till 96; `*/
-		if (digit == 57)
-			digit += 39;
+		if (numberz == 57)
+			numberz += 39;
 
-		++digit;
+		++numberz;
 	}
 	putchar('\n');
 
