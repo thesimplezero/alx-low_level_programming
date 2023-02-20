e8df38f5215af4b9734c726198fa8b2bc0873750
thesimplezero <hi@thesimplezero>
2023-02-20 23:10:08 +0300

    alpha

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,25 +6,26 @@
 /**
  * main - Entry point
  *
- * Description: print value of n satus; zero, positive or negative
+ * Description: print value of n status; zero, positive or negative
  *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-	int num;
+	int n;
 
 	srand(time(0));
-	num = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
-	/*my code goes here*/
-	if (num > 0)
-		printf("%i is positive\n", num);
-	else if (num == 0)
-		printf("%i is zero\n", num);
-	else
-		printf("%i is negative\n", num);
+	if (abs(n) == 0)
+		printf("%i is zero\n", n);
+	else if (abs(n) > 0)
+	{
+		if (n > 0)
+			printf("%i is positive\n", n);
+		else
+			printf("%i is negative\n", n);
+	}
 
 	return (0);
 }
