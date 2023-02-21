493d2a222039c9dd25db292ea4e84259f6bb1860
thesimplezero <hi@thesimplezero>
2023-02-22 01:33:28 +0300

    sum of first 98 fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: Finds and prints the first 98 Fibonacci numbers, starting
- * with 1 and 2, separated by commas and spaces.
- *
- * Return: Always 0 (success)
- */
+* main - Entry point of the program
+*
+* Description: Finds and prints the sum of the even-valued terms in the
+* Fibonacci sequence whose values do not exceed 4,000,000.
+*
+* Return: Always 0 (success)
+*/
 
 int main(void)
 {
-    int i, fib1 = 1, fib2 = 2, fib3; /* Initialize the first two Fibonacci numbers */
-    
-    printf("%d, %d", fib1, fib2); /* Print the first two Fibonacci numbers */
+int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 0;
 
-    /* Calculate and print the next 96 Fibonacci numbers */
-    for (i = 3; i <= 98; i++)
-    {
-        fib3 = fib1 + fib2; /* Calculate the next Fibonacci number */
-        printf(", %d", fib3); /* Print the number separated by a comma and space */
-        fib1 = fib2; /* Update the previous two Fibonacci numbers */
-        fib2 = fib3;
-    }
+/* Loop through the Fibonacci sequence */
+while (fib_sum <= 4000000)
+{
+fib_sum = fib1 + fib2; /* Calculate the next Fibonacci number */
 
-    printf("\n"); /* Print a new line character */
-    return 0; 
+/* Check if the Fibonacci number is even */
+if (fib_sum % 2 == 0)
+even_sum += fib_sum; /* Add it to the sum of even-valued terms */
+
+/* Update the previous two Fibonacci numbers */
+fib1 = fib2;
+fib2 = fib_sum;
+}
+
+printf("%d\n", even_sum); /* Print the sum of even-valued terms */
+
+return (0);
 }
