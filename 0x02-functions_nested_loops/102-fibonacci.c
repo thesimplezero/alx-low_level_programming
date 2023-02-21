b5486c3b8539c7dced24c409c87bb8b4eaa76f53
thesimplezero <hi@thesimplezero>
2023-02-22 01:24:48 +0300

    paper route

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,33 +1,32 @@
-#include <stdio.h>
+#include"main.h"
 
 /**
-* main - Prints the first 50 Fibonacci numbers in the set
-*
-* Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Description: prints the first 50 Fibonacci numbers
+ *		starting with 1 and 2 folled by a new line
+ *
+ * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-int count = 0;
-long int fib1 = 1, fib2 = 2, fib;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-/* Print the first 50 Fibonacci numbers in the set */
-while (count < 50)
-{
-fib = fib1 + fib2; /* Calculate the next Fibonacci number */
+	for (count = 0; count < 50; ++count)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-if (fib <= 20365011074) /* Check if the Fibonacci number is in the set */
-{
-if (count != 0)
-printf(", ");
+		fib1 = fib2;
+		fib2 = sum;
 
-printf("%ld", fib); /* Print the Fibonacci number */
-count++; /* Increment the count of printed Fibonacci numbers */
-}
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
 
-fib1 = fib2; /* Update the previous two Fibonacci numbers */
-fib2 = fib;
-}
-
-printf("\n"); /* Print a new line character */
-return (0);
+	return (0);
 }
