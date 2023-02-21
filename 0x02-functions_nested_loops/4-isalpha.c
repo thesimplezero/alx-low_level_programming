f92c3951bf2318be98217c7c93287654e8a68f6c
thesimplezero <hi@thesimplezero>
2023-02-21 16:53:11 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,14 +8,15 @@
  * Return: 1 if `c` is a letter, lowercase or uppercase,
  *         0 otherwise.
  */
+
 int _isalpha(int c)
 {
     if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
     {
-        return 1;
+        return (1);
     }
     else
     {
-        return 0;
+        return (0);
     }
 }
