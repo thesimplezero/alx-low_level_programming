b85ba3859e20e98e23106578e977433320c26bea
thesimplezero <hi@thesimplezero>
2023-02-23 15:45:46 +0300

    made a few edits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0 - 9
- *             only using _putchar twice
- *
- * Return: Always 0 (Success)
+* print_numbers - print 0 - 9
+*             only using _putchar twice
+*
+* Return: Always 0 (Success)
 */
 
-void print_numbers()
+void print_numbers(void)
 {
-	int num;
+int num;
 
-	for (num = 0; num <= 9; ++num)
-	{
-		if (num == 2 || num == 4)
-			continue;
-		_putchar(num + 48);
-	}
-	_putchar('\n');
+for (num = 0; num <= 9; ++num)
+{
+_putchar(num + 48);
+}
+_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print 0 - 9 apart
- *                 from 2 and 4 and you can
- *                 only use _putchar twice
- * Return: Always 0 (Success)
+* print_most_numbers - print 0 - 9 apart
+*                 from 2 and 4 and you can
+*                 only use _putchar twice
+* Return: Always 0 (Success)
 */
 
 void print_most_numbers(void)
 {
-	int num = 0;
-	while (num <= 9)
-	{
-		if (num != 2 && num != 4)
-		{
-			_putchar(num + 48);
-		}
-	num++;
-	}
-	_putchar('\n');
+int num = 0;
+while (num <= 9)
+{
+if (num != 2 && num != 4)
+{
+_putchar(num + 48);
+}
+num++;
+}
+_putchar('\n');
 }
