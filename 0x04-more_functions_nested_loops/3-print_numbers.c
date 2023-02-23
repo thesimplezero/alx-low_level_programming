29247ad607d52b8c33360ca940ec83c22f9b6caf
thesimplezero <hi@thesimplezero>
2023-02-23 15:42:24 +0300

    I believe 😁

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers()
+{
+	int num;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
