b8c78727ec9c39e8e0bba91fafc6ab0de5150e3c
thesimplezero <hi@thesimplezero>
2023-02-27 23:31:38 +0300

    stylistic consequences of the components and systems

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+/**
+ * rev_string - reverses a string
+ *
+ * @s: pointer to the string to be reversed
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i;
+	char ch;
+
+	/* Find the length of the string without null character */
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+
+	/* Swap the string by looping to half the string */
+	for (i = 0; i < len / 2; ++i)
+	{
+		ch = *(s + i);
+		*(s + i) = *(s + len - 1 - i);
+		*(s + len - 1 - i) = ch;
+	}
+}
