d0d0dd9c2cd6981f6de53924a8eb6662a45fea21
thesimplezero <hi@thesimplezero>
2023-02-27 23:43:05 +0300

    6-puts edit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,21 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_even_chars - prints every even character of a string
+ * puts2 - prints every character of a string
  *
- * @str: string to print even characters from
+ * @str: string parameter input
  *
- * Return: nothing
- */
-void print_even_chars(char *str)
-{
-        int i;
+ * Return: Nothing
+*/
 
-        for (i = 0; str[i] != '\0'; ++i)
-        {
-                if (i % 2 == 0)
-                        _putchar(str[i]);
-        }
-        _putchar('\n');
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
 }
