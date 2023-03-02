171e31928e3e86c3573012bdb5308d6ea638d63f
thesimplezero <hi@thesimplezero>
2023-03-03 01:15:17 +0300

    spk

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,21 @@
+#include "main.h"
 /**
- * _strcmp - compare two strings
- * @s1: pointer to first string
- * @s2: pointer to second string
- *
- * Return: integer value indicating the difference between the two strings
- */
-int _strcmp(char *s1, char *s2)
+* _strcmp - Compares two strings
+* @st1: input
+* @st2: input
+* Return: dest
+*/
+int _strcmp(char *st1, char *st2)
 {
-        int i = 0;
+int i = 0;
 
-        while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
-        {
-                if (*(s1 + i) != *(s2 + i))
-                        return (*(s1 + i) - *(s2 + i));
-                i++;
-        }
-
-        return (*(s1 + i) - *(s2 + i));
+while (st1[i] != '\0' && st2[i] != '\0')
+{
+if (st1[i] != st2[i])
+{
+return (st1[i] - st2[i]);
+}
+i++;
+}
+return (0);
 }
