fea6d2444b54abb91d4c3f504720bf2652979ccc
thesimplezero <hi@thesimplezero>
2023-03-03 01:10:22 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strcmp.c -o 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,20 @@
 /**
- * _strcmp - compares two strings
+ * _strcmp - compare two strings
  * @s1: pointer to first string
  * @s2: pointer to second string
  *
- * Return: negative integer if s1 < s2, 0 if s1 == s2,
- *         positive integer if s1 > s2
+ * Return: integer value indicating the difference between the two strings
  */
 int _strcmp(char *s1, char *s2)
 {
         int i = 0;
 
-        while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+        while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
+        {
+                if (*(s1 + i) != *(s2 + i))
+                        return (*(s1 + i) - *(s2 + i));
                 i++;
+        }
 
-        return (s1[i] - s2[i]);
+        return (*(s1 + i) - *(s2 + i));
 }
