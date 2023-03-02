b18e84515075738ebb1fdd71fc9f117914bad9f9
thesimplezero <hi@thesimplezero>
2023-03-03 01:28:11 +0300

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,25 @@
 /**
-* leet - encodes a string into 1337
-* @s: string to be encoded
-*
-* Return: pointer to encoded string
-*/
-char *leet(char *s)
+ * leet - Encodes a string into 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *str)
 {
-int i, j;
-char letters[] = "aeotlAEOTL";
-char numbers[] = "4307143071";
+	int i, j;
+	char letters[] = "aeotlAEOTL";
+	char numbers[] = "4307143071";
 
-for (i = 0; s[i]; i++)
-{
-for (j = 0; letters[j]; j++)
-{
-if (s[i] == letters[j])
-{
-s[i] = numbers[j];
-break;
-}
-}
-}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = numbers[j];
+			}
+		}
+	}
 
-return s;
+	return (str);
 }
