d0ffb9da6823d62c9fb1b2ff59989ec474bf216b
thesimplezero <hi@thesimplezero>
2023-03-02 21:16:24 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes from src to concatenate
+ *
+ * Return: pointer to resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int dest_len = 0;
+    int i;
+
+    while (dest[dest_len] != '\0')
+    {
+        dest_len++;
+    }
+
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        dest[dest_len + i] = src[i];
+    }
+
+    dest[dest_len + i] = '\0';
+
+    return dest;
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strncpy - copy a string from src to dest up to n characters
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the maximum number of characters to copy
+ *
+ * Return: a pointer to the destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i;
+
+    for (i = 0; i < n && src[i] != '\0'; i++)
+        dest[i] = src[i];
+
+    for (; i < n; i++)
+        dest[i] = '\0';
+
+    return dest;
+}
