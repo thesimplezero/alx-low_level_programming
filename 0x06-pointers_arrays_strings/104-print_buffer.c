84a44c881e36641b030436bf65c35ec4b22172ba
thesimplezero <hi@thesimplezero>
2023-03-03 00:53:40 +0300

    Noise is a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,41 +1,43 @@
+#include "main.h"
 #include <stdio.h>
 
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer.
+ * @size: size of buffer.
+ * Return: no return.
+ */
 void print_buffer(char *b, int size)
 {
-    int i, j;
-    unsigned char c;
+	int j, k, l;
 
-    if (size <= 0) {
-        printf("\n");
-        return;
-    }
-
-    for (i = 0; i < size; i += 10) {
-        printf("%08x: ", i);
-
-        for (j = 0; j < 10; j++) {
-            if (i + j < size) {
-                printf("%02x", b[i + j]);
-            } else {
-                printf("  ");
-            }
-
-            printf(" ");
-        }
-
-        for (j = 0; j < 10; j++) {
-            if (i + j < size) {
-                c = b[i + j];
-                if (c < 32 || c > 126) {
-                    printf(".");
-                } else {
-                    printf("%c", c);
-                }
-            } else {
-                printf(" ");
-            }
-        }
-
-        printf("\n");
-    }
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
+	}
 }
