1c0be53e313187f3f850c3458a0afc02b6031943
thesimplezero <hi@thesimplezero>
2023-03-09 01:02:42 +0300

    inception 👿

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: base number.
+ * @y: exponent.
+ *
+ * Return: x raised to the power of y.
+ *         -1 if y is negative.
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+        return (-1);
+    if (y == 0)
+        return (1);
+    if (y == 1)
+        return (x);
+    return (x * _pow_recursion(x, y - 1));
+}
