f3b558579cafe3bf8cc77efb30896ff8e8f1f6fd
thesimplezero <hi@thesimplezero>
2023-03-10 18:11:04 +0300

    It ain't what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments passed to the program
+ * @argv: array of pointers to the arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+    if (argc >= 1)
+    {
+        printf("%s\n", argv[0]);
+    }
+
+    return (0);
+}
