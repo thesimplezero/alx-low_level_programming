239f266ace3ef7dd22a1cacf12cb11fec16911eb
thesimplezero <hi@thesimplezero>
2023-03-15 01:48:54 +0300

    2-str_concat.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,35 +7,34 @@
  * @width: width
  * @height: height
  *
- * Return: NULL on faliure
-*/
+ * Return: NULL on failure
+ */
 
 int **alloc_grid(int width, int height)
 {
-	int **grid, i, j;
+    int **grid;
+    int i, j;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
+    if (width <= 0 || height <= 0)
+        return (NULL);
 
-	grid = malloc(height * sizeof(int *));
-	if (grid == NULL)
-		return (NULL);
+    grid = malloc(height * sizeof(int *));
+    if (grid == NULL)
+        return (NULL);
 
-	for (i = 0; i < height; i++)
-	{
-		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
-		{
-			while (i >= 0)
-			{
-				free(grid[i]);
-				i--;
-			}
-			free(grid);
-			return (NULL);
-		}
-		for (j = 0; j < width; j++)
-			grid[i][j] = 0;
-	}
-	return (grid);
+    for (i = 0; i < height; i++)
+    {
+        grid[i] = malloc(width * sizeof(int));
+        if (grid[i] == NULL)
+        {
+            for (j = 0; j < i; j++)
+                free(grid[j]);
+            free(grid);
+            return (NULL);
+        }
+        for (j = 0; j < width; j++)
+            grid[i][j] = 0;
+    }
+
+    return (grid);
 }
