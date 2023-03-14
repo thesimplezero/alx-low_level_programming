34c6f879e95c8381727d29a55cf8b31ff7e15dd5
thesimplezero <hi@thesimplezero>
2023-03-15 01:22:49 +0300

    2-str_concat.c

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,19 +6,18 @@
  * main - check the code for ALX School students.
  *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
-	char *s;
+    char *s;
 
-	s = str_concat("Betty ", "Holberton");
-	if (s == NULL)
-	{
-		printf("failed\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-	return (0);
+    s = str_concat("Betty ", "Holberton");
+    if (s == NULL)
+    {
+        printf("failed\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,41 @@
 #include <stdlib.h>
 
 /**
- * str_concat - a function that concatenates two strings.
+ * str_concat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
  *
- * @s1: input to string 1
- * @s2: input to string 2
- *
- * Return: NULL on faliure
-*/
-
+ * Return: Pointer to concatenated string or NULL on failure
+ */
 char *str_concat(char *s1, char *s2)
 {
-	int a = 0, b = 0;
-	int i, j;
-	char *s;
+        unsigned int len1 = 0, len2 = 0, i, j;
+        char *s;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+        if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
 
-	/*find length of str1 & str2*/
-	while (s1[a] != '\0')
-		a++;
-	while (s2[b] != '\0')
-		b++;
+        /* Find length of s1 and s2 */
+        while (s1[len1])
+                len1++;
+        while (s2[len2])
+                len2++;
 
-	/*+1 for our end of string character*/
-	s = malloc((a * sizeof(char)) + ((b + 1) * sizeof(char)));
+        s = malloc((len1 + len2 + 1) * sizeof(char));
+        if (s == NULL)
+                return (NULL);
 
-	if (s == NULL)
-		return (NULL);
+        /* Copy s1 to s */
+        for (i = 0; s1[i]; i++)
+                s[i] = s1[i];
 
-	/*add the first string to array s*/
-	for (i = 0; s1[i] != '\0'; i++)
-		s[i] = s1[i];
-	/*add the second string to array s*/
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		s[i] = s2[j];
-		i++;
-	}
+        /* Copy s2 to s */
+        for (j = 0; s2[j]; j++)
+                s[i + j] = s2[j];
 
-	/*null terminate our new string*/
-	s[i] = '\0';
+        s[i + j] = '\0';
 
-	return (s);
+        return (s);
 }
