33f3fd36fb32c6c098aa785c38045355433ba18c
thesimplezero <hi@thesimplezero>
2023-03-17 00:39:49 +0300

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: amount of bytes
+ *
+ * Return: pointer to our new allocated memory or NULL if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *new_mem;
+
+    new_mem = malloc(b);
+    if (new_mem == NULL)
+    {
+        return (NULL);
+    }
+    return (new_mem);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: input pointer that represents memory
+ *     block to fill
+ * @b: characters to fill
+ * @n: number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory area
+*/
+void *_memset(void *s, int b, size_t n)
+{
+    unsigned char *p = s;
+    while (n--)
+        *p++ = (unsigned char) b;
+    return s;
+}
+
+/**
+ * _calloc - allocates memory for an array of nmemb elements
+ * of size bytes each and returns a pointer to the allocated memory.
+ *
+ * The memory is set to zero.
+ *
+ * @nmemb: number of elements
+ * @size: size in bytes of each element
+ *
+ * Return: pointer to the allocated memory or NULL if it fails
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    void *new_mem = NULL;
+
+    if (nmemb == 0 || size == 0)
+        return NULL;
+
+    new_mem = malloc(nmemb * size);
+    if (new_mem == NULL)
+        return NULL;
+
+    _memset(new_mem, 0, nmemb * size);
+
+    return new_mem;
+}
