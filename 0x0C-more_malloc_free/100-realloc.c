ad156839396bdaf7e42afcb0117da9018e93a772
thesimplezero <hi@thesimplezero>
2023-03-17 00:44:11 +0300

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,89 +2,89 @@
 #include <stdio.h>
 
 /**
- * _realloc - reallocate a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated
- * @old_size: the size, in bytes, of the allocated space for ptr
- * @new_size: the new size, in bytes, of the new memory block
- *
- * Return: pointer to the new allocated memory block
- */
+* _realloc - reallocate a memory block using malloc and free
+* @ptr: pointer to the memory previously allocated
+* @old_size: the size, in bytes, of the allocated space for ptr
+* @new_size: the new size, in bytes, of the new memory block
+*
+* Return: pointer to the new allocated memory block
+*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_ptr;
-	unsigned int i;
+char *new_ptr;
+unsigned int i;
 
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
 
-	if (new_size == old_size)
-		return (ptr);
+if (new_size == old_size)
+return (ptr);
 
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-		if (ptr == NULL)
-			return (NULL);
-		return (ptr);
-	}
+if (ptr == NULL)
+{
+ptr = malloc(new_size);
+if (ptr == NULL)
+return (NULL);
+return (ptr);
+}
 
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-		return (NULL);
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+return (NULL);
 
-	for (i = 0; i < old_size && i < new_size; i++)
-		new_ptr[i] = ((char *)ptr)[i];
+for (i = 0; i < old_size && i < new_size; i++)
+new_ptr[i] = ((char *)ptr)[i];
 
-	free(ptr);
+free(ptr);
 
-	return (new_ptr);
+return (new_ptr);
 }
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
+* simple_print_buffer - prints buffer in hexa
+* @buffer: the address of memory to print
+* @size: the size of the memory to print
+*
+* Return: Nothing.
+*/
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i;
+unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-			printf(" ");
-		if (!(i % 10) && i)
-			printf("\n");
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
+i = 0;
+while (i < size)
+{
+if (i % 10)
+printf(" ");
+if (!(i % 10) && i)
+printf("\n");
+printf("0x%02x", buffer[i]);
+i++;
+}
+printf("\n");
 }
 
 /**
- * main - check the code for
- *
- * Return: Always 0.
- */
+* main - check the code for
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-	char *p;
-	unsigned int i;
+char *p;
+unsigned int i;
 
-	p = malloc(sizeof(char) * 10);
-	p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
-	i = 0;
-	while (i < 98)
-	{
-		p[i++] = 98;
-	}
-	simple_print_buffer(p, 98);
-	free(p);
-	return (0);
+p = malloc(sizeof(char) * 10);
+p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
+i = 0;
+while (i < 98)
+{
+p[i++] = 98;
+}
+simple_print_buffer(p, 98);
+free(p);
+return (0);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,28 +1,28 @@
 #include <stdlib.h>
 
 /**
- * array_range - creates an array of integers
- *               containing all values between min and max inclusive.
- *
- * @min: minimum value of the array
- * @max: maximum value of the array
- *
- * Return: pointer to the newly created array
- *         NULL if failed
- */
+* array_range - creates an array of integers
+*               containing all values between min and max inclusive.
+*
+* @min: minimum value of the array
+* @max: maximum value of the array
+*
+* Return: pointer to the newly created array
+*         NULL if failed
+*/
 int *array_range(int min, int max)
 {
-    int i, *arr;
+int i, *arr;
 
-    if (min > max)
-        return (NULL);
+if (min > max)
+return (NULL);
 
-    arr = malloc(sizeof(int) * (max - min + 1));
-    if (arr == NULL)
-        return (NULL);
+arr = malloc(sizeof(int) * (max - min + 1));
+if (arr == NULL)
+return (NULL);
 
-    for (i = 0; min <= max; i++, min++)
-        arr[i] = min;
+for (i = 0; min <= max; i++, min++)
+arr[i] = min;
 
-    return (arr);
+return (arr);
 }
