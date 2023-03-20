3d71f6bfcdbbc23ec35a7961687a1b98a54b2734
thesimplezero <hi@thesimplezero>
2023-03-21 01:07:45 +0300

    added docs

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS
-#define ABS(x) ((x < 0) ? (-x) : (x))
-#endif /* ABS */
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (((x) >= 0) ? (x) : -(x))
+
+#endif /* _FUNCTION_LIKE_MACRO_H */
