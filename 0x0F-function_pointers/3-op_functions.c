8a401e4f69dca2d1a5b297c86172f665e1230e2b
thesimplezero <hi@thesimplezero>
2023-03-23 01:24:11 +0300

    Updating 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+* op_add - Adds two integers
+* @a: First integer
+* @b: Second integer
+*
+* Return: Sum of a and b
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub - Subtracts two integers
+* @a: First integer
+* @b: Second integer
+*
+* Return: Difference between a and b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* op_mul - Multiplies two integers
+* @a: First integer
+* @b: Second integer
+*
+* Return: Product of a and b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* op_div - Divides two integers
+* @a: First integer
+* @b: Second integer
+*
+* Return: Result of dividing a by b
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+* op_mod - Finds remainder of division of two integers
+* @a: First integer
+* @b: Second integer
+*
+* Return: Remainder of dividing a by b
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
