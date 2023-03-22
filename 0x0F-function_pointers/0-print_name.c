a1a876cfd5258ea28a8ad7a5554dcdbd401c7e28
thesimplezero <hi@thesimplezero>
2023-03-23 01:45:21 +0300

    Pushing 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * print_name - function to print name
- *
- * @name: name to print
- * @f: function pointer that doesn't return anything
- *
- * Return: Always void
- */
+* print_name - function to print name
+*
+* @name: name to print
+* @f: function pointer that doesn't return anything
+*
+* Return: Always void
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+if (name != NULL && f != NULL)
+f(name);
 }
